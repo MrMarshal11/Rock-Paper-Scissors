e079955797560891ed4fbcf0788250d7dec47a3d
Mr.Marshal <marshalrock11@gmail.com>
2024-08-07 16:18:15 +1000

    Fixed two getHumanChoice function

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-// Create a new function named getComputerChoice.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// Hint: The Math.random method returns a random number that’s greater than or equal to 0 and less than 1. Think about how you can use this to conditionally return one of the multiple choices.
-// Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
-
-let result = prompt('Rock, Paper, Scissors')
-
-function getComputerChoice(choice) {
+function getComputerChoice(computerChoice) {
     let num = Math.random()
         if (num <= 0.33) {
             console.log('Rock')
@@ -18,8 +11,13 @@ function getComputerChoice(choice) {
         }
 }
 
-if (result != "rock" && result != "paper" && result != "scissors") {
+function getHumanChoice(humanChoice) {
+    let result = prompt('Rock, Paper, Scissors!')
+    if (result != "rock" && result != "paper" && result != "scissors") {
     console.log('thefucareyoudoingmate')
-}   else {
+    }   else {
     console.log(getComputerChoice())
+    }
 }
+
+getHumanChoice()
